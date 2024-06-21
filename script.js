5c6f9afab00e70c10f88d17646d9c2fb7b8800d5
Hooolymax <volitantandre@gmail.com>
2024-06-21 16:43:28 -0400

    upd

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('booking-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var date = document.getElementById('date').value;
+    var time = document.getElementById('time').value;
+    var result = document.getElementById('result');
+    if (date && time) {
+        result.innerHTML = `You have reserved ${date} ${time} for the event.`;
+        alert("Reserving successful."); // 
+    } else {
+        result.innerHTML = "Please fill in all fields.";
+        alert("Please fill in all fields."); //
+    }
+});
